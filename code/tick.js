d235568a2cbf05bdd6ba747a0b5ffe68dd497611
LynnScarlett/T470 <artzyh@163.com>
2018-10-10 19:37:14 +0800

    tick

--- code/tick.js
+++ code/tick.js
@@ -7,16 +7,16 @@ process.nextTick(function () {
 });
 
 // 加入两个setImmediate()的回调函数
-setImmediate(function () {
-  console.log('setImmediate延迟执行1');
+setTimeout(function () {
+  console.log('setTimeout延迟执行1');
   // 进入下次循环
   process.nextTick(function () {
     console.log('强势插入');
   });
-});
+},0);
 
 setImmediate(function () {
   console.log('setImmediate延迟执行2');
-});
+},0);
 
 console.log('正常执行');
